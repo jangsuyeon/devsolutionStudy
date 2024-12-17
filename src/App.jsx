b7d45c3ae87a008f710c44d4af65c26e628c4cc2
Kejr314 <ejrdud314@gmail.com>
2024-12-17 20:04:44 +0900

    [ADD] 프로젝트 관리 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Index from './Index'
+import ProjectList from './project/ProjectList'
+import ProjectAdd from './project/projectAdd'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Index />} />
+        
+        <Route path='/project' element={<ProjectList />} />
+        <Route path='/project/add' element={<ProjectAdd />} />
       </Routes>
     </>
   )
