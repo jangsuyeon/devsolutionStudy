9c5e1a7a5b1948f72eddb6eec8b7fbf306ef1ea7
hongyeeun <yeeun5023@gmail.com>
2024-12-27 14:49:21 +0900

    [MOD] 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-
+import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Index from './Index'
-import Layout from './dashboard/Layout'
-import DashBoard from './dashboard/DashBoard'
-
+import ProjectList from './project/ProjectList'
+import ProjectAdd from './project/projectAdd'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Index />} />
-        <Route path='/layout' element={<Layout />} />
-        <Route path='/dashboard' element={<DashBoard />} />
+        
+        <Route path='/project' element={<ProjectList />} />
+        <Route path='/project/add' element={<ProjectAdd />} />
       </Routes>
     </>
   )
