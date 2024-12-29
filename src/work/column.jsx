b25e09070633c926a501cf7ff2f219fad77b3e35
jwkim-portable <kh.jwkim@gmail.com>
2024-12-29 17:40:37 +0900

    [ADD] Drag&Drop 가능한 리스트 컴포넌트 구현

---
+++ src/work/column.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
+import Task from './task';
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+`;
+const Title = styled.h3`
+    padding: 8px;
+`;
+const TaskList = styled.div`
+    padding: 8px;
+    background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
+`;
+
+function Column(props) {
+    return (
+        <>
+            <Container>
+                <Title>{props.column.title}</Title>
+                <Droppable droppableId={props.column.id}>
+                    {(provided, snapshot) => (
+                        <TaskList
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                            isDraggingOver={snapshot.isDraggingOver}
+                        >
+                            {props.tasks.map((task, index) => (
+                                <Task key={task.id} task={task} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </TaskList>
+                    )}
+                </Droppable>
+            </Container>
+        </>
+    )
+}
+
+export default Column;
